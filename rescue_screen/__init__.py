4b9a876a138d702115a4d02ccc7012e6d36a8600
Kontuch Suksawat <zeroxr20@gmail.com>
2025-03-09 01:31:17 +0700

    add: admin nav to phine management

--- rescue_screen/__init__.py
+++ rescue_screen/__init__.py
@@ -25,5 +25,6 @@ from .safty_tips_management import Tips_page, EditTipScreen, CreateTipScreen
 from .salfty_tips import Tips_page
 from .SymbolPage import SymbolScreen
 from .MyAdminDevelopPage import MyAdminDevelop
+from .card_page import Card_page, EditCardScreen, CreateCardScreen
 
 # from .BottonNavItem import BottomNavBar
